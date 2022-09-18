0313f7c80bd0302e995b10932fbb59543b4141bd
Ciro Framulari <ciroframulari@gmail.com>
2022-09-18 17:20:25 -0300

    Trabajo Practico Semana 5

---
+++ Clase9/02-variables.js
@@ -0,0 +1,15 @@
+// VARIABLES
+// Una variable es un contenedor de informacion
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = "2021";
+var pais_y_continente_y_antiguedad = pais + " " + continente + " " + antiguedad;
+
+var pais = "México";
+var continente2 = "Latinoamerica";
+var pais_y_continente2 = pais + " es parte de " + continente2;  
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente_y_antiguedad);
+alert(pais_y_continente2);
